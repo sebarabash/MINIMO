518ffd3dddb0bad88c69b44b87b799e66de8fcb6
sebarabash <62248308+sebarabash@users.noreply.github.com>
2020-10-12 19:52:49 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+$(document).ready(function () {
+$(".menu__burger").click(function(event){
+    $(".menu__burger").toggleClass("menu__burger-active");
+    $(".menu__list").toggleClass("menu__list-active");
+    $("body").toggleClass("lock");
 
+})  
+});
